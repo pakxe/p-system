a413391ed0b0f12d23b93d55e0a25715f315cbb5
pakxe <pigkill40@naver.com>
2025-01-13 13:26:12 +0900

    chore: 3d 모델 파일 인지할 수 있도록 설정 파일 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import glsl from 'vite-plugin-glsl';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    glsl(),
     react({
       jsxImportSource: '@emotion/react',
       babel: {
@@ -11,4 +13,5 @@ export default defineConfig({
       },
     }),
   ],
+  assetsInclude: ['**/*.gltf', '**/*.glb'],
 });
