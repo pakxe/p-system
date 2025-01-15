dc164361605584bd6f7c6195d8b0c94f34f32ae8
pakxe <pigkill40@naver.com>
2025-01-15 18:14:39 +0900

    feat: 프로젝트에서 사용되는 타입 분리

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+import { Vector3 } from 'three';
+
+type OrbitalCenter = Vector3;
+
+// 항성 | 생성 = 시스템 내 객체 = system object
+export type TSystemObject = {
+  name: string;
+  mainColor: string;
+  planetRadius: number;
+  orbitalRadius: number; // 타원 궤도는 이 계에 없다고 가정한다.. 따라서 장, 단 반지름이 아닌 하나의 반지름만 사용한다.
+  orbitalSpeed: number; // 각속도임
+  rotationSpeed: number;
+  orbitalCenter: OrbitalCenter;
+};
+
+export type TPlanet = TSystemObject & {
+  satellites?: TSatellite[];
+};
+
+export type TSatellite = Omit<TPlanet, 'satellites' | 'orbitalCenter'> & {};
+
+export type TOrbit = {
+  orbitalCenter: OrbitalCenter;
+  orbitalRadius: number;
+};
