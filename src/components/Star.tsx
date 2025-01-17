b18797422d2ffe5245eb94e9137d78fcce568195
pakxe <pigkill40@naver.com>
2025-01-17 17:35:22 +0900

    feat: Star 컴포넌트(목업모델 끼워서)

---
+++ src/components/Star.tsx
@@ -0,0 +1,46 @@
+import { Mesh } from 'three';
+import { TStar } from '../types';
+import Model from './Model';
+import { useRef } from 'react';
+import useRotation from '../hooks/useRotation';
+
+type Props = TStar & {
+  modelName?: string;
+  isCenter?: boolean;
+
+  centerRef?: React.RefObject<Mesh>;
+  onClick: (ref: React.RefObject<Mesh>, name: string) => void;
+  onExplore: () => void;
+};
+
+// 아직까지 star는 반드시 중심에 있는다.
+const Star = ({ onClick, name, modelName, axialTilt, rotationSpeed, objectRadius }: Props) => {
+  const planetRef = useRef<Mesh>(null);
+
+  useRotation({ planetRef, axialTilt, rotationSpeed });
+
+  return (
+    <group>
+      <mesh
+        ref={planetRef}
+        onClick={() => {
+          if (onClick) onClick(planetRef, name);
+        }}>
+        {modelName ? (
+          // <Model name={modelName} />
+          <Model />
+        ) : (
+          <>
+            <sphereGeometry args={[objectRadius, 32, 32]} />
+            <meshStandardMaterial
+              emissive='#ffffff' // 발광 색상
+              emissiveIntensity={0.2} // 발광 강도
+            />
+          </>
+        )}
+      </mesh>
+    </group>
+  );
+};
+
+export default Star;
