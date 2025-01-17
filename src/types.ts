0b97ab5e370d85bbddd84f5b404aeb3cf3df7621
pakxe <pigkill40@naver.com>
2025-01-17 17:34:04 +0900

    type: Star 타입 추가

--- src/types.ts
+++ src/types.ts
@@ -6,13 +6,15 @@ type OrbitalCenter = Vector3;
 export type TSystemObject = {
   name: string;
   mainColor: string;
-  planetRadius: number;
+  objectRadius: number;
   orbitalRadius: number; // 타원 궤도는 이 계에 없다고 가정한다.. 따라서 장, 단 반지름이 아닌 하나의 반지름만 사용한다.
   orbitalSpeed: number; // radian per frame
   rotationSpeed: number;
   orbitalCenter: OrbitalCenter;
   axialTilt: number; // 각도. 라디안 아님
 };
+
+export type TStar = Omit<TSystemObject, 'orbitalCenter' | 'orbitalRadius' | 'satellites' | 'orbitalSpeed'>;
 
 export type TPlanet = TSystemObject & {
   satellites?: TSatellite[];
