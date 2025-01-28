a364c43c9338976efc8a0584f209596b2314b415
pakxe <pigkill40@naver.com>
2025-01-29 02:33:12 +0900

    feat: 보려는 모델의 위치와 사이즈를 받아 적절한 위치로 카메라를 이동시키는 SingleCamera 컴포넌트 구현

---
+++ src/pages/AssemblyPage/components/SingleCamera.tsx
@@ -0,0 +1,22 @@
+import { useThree } from '@react-three/fiber';
+import { PerspectiveCamera } from 'three';
+import { VECTOR_3 } from '../../../types';
+import * as THREE from 'three';
+
+type Props = {
+  position: THREE.Vector3;
+  size: number;
+};
+
+const SingleCamera = ({ position, size }: Props) => {
+  const { camera }: { camera: PerspectiveCamera } = useThree();
+
+  const distance = size / 2;
+  // 포지션에서 +z, +y, +x방향으로 20씩 떨어진 위치에서 본다,
+  camera.position.set(position.x + distance, position.y + distance, position.z + distance);
+  camera.lookAt(position);
+
+  return null;
+};
+
+export default SingleCamera;
