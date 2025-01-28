5d2bca012826adfca3cb00d5206a548c004ecb37
pakxe <pigkill40@naver.com>
2025-01-29 02:16:44 +0900

    feat: 테마 프로바이더 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { GlobalStyle } from './GlobalStyle';
-import { Global } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './Router';
+import theme from './theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    <Global styles={GlobalStyle} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+      <Global styles={GlobalStyle} />
+    </ThemeProvider>
   </StrictMode>,
 );
