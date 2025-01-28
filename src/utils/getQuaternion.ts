7718433ad7102b1a6fb5390a677eaa8aa641f88e
pakxe <pigkill40@naver.com>
2025-01-28 18:46:27 +0900

    fix: 인자로 받는 벡터 모두 정규화

--- src/utils/getQuaternion.ts
+++ src/utils/getQuaternion.ts
@@ -5,10 +5,10 @@ import * as THREE from 'three';
  * 기준 벡터와 이동하려는 벡터를 받아 쿼터니언을 반환하는 함수
  */
 const getQuaternion = (baseDirection: VECTOR_3, targetDirection: VECTOR_3) => {
+  const defaultDirection = new THREE.Vector3(...baseDirection).normalize();
   const direction = new THREE.Vector3(...targetDirection).normalize();
 
   const quaternion = new THREE.Quaternion();
-  const defaultDirection = new THREE.Vector3(...baseDirection);
   quaternion.setFromUnitVectors(defaultDirection, direction);
 
   return quaternion;
