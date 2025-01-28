8ef6f7b8253d8d81c8a327e016d8ef8468cf421f
pakxe <pigkill40@naver.com>
2025-01-28 19:48:49 +0900

    feat: 스텝 변경에 따라 시선과 위치를 부드럽게 보간하도록 함

--- src/pages/AssemblyPage/components/StepCamera.tsx
+++ src/pages/AssemblyPage/components/StepCamera.tsx
@@ -1,11 +1,18 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { useFrame, useThree } from '@react-three/fiber';
 import { PerspectiveCamera, Vector3 } from 'three';
+import { VECTOR_3 } from '../../../types';
 
-const StepCamera = ({ cameraPosition }: { cameraPosition: [number, number, number] }) => {
+type Props = {
+  cameraPosition: VECTOR_3;
+  isCameraMoving: boolean;
+  setIsCameraMoving: (value: boolean) => void;
+};
+
+const StepCamera = ({ cameraPosition, setIsCameraMoving, isCameraMoving }: Props) => {
   const { camera }: { camera: PerspectiveCamera } = useThree();
-  const [isCameraMoving, setIsCameraMoving] = useState(false);
   const targetPos = useRef(new Vector3(...cameraPosition));
+
   const lerpSpeed = 0.04;
   const threshold = 0.1;
 
@@ -17,10 +24,20 @@ const StepCamera = ({ cameraPosition }: { cameraPosition: [number, number, numbe
   useFrame(() => {
     if (!isCameraMoving) return;
 
-    camera.position.copy(camera.position.lerp(targetPos.current, lerpSpeed));
+    // 카메라 위치 업데이트
+    camera.position.lerp(targetPos.current, lerpSpeed);
 
+    // 카메라 시선 업데이트
+    const target = camera.clone();
+    target.lookAt(0, 0, 0);
+
+    camera.quaternion.slerp(target.quaternion, 0.9);
+
+    // 목표 위치에 근접하면 이동 중단
     if (camera.position.distanceTo(targetPos.current) < threshold) {
       camera.position.copy(targetPos.current);
+      camera.lookAt(0, 0, 0);
+
       setIsCameraMoving(false);
     }
   });
