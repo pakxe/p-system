6e5752951e13c142d128575353a1e1cb2b24d1be
pakxe <pigkill40@naver.com>
2025-01-28 17:18:24 +0900

    feat: 불필요한 서스펜스와 바운더리 제거

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,6 @@
 import { createBrowserRouter, Outlet } from 'react-router-dom';
 import App from './App';
 import AssemblyPage from './pages/AssemblyPage/AssemblyPage';
-import { Suspense } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
 
 const router = createBrowserRouter([
   {
@@ -15,13 +13,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: (
-          <ErrorBoundary FallbackComponent={({ error }) => <div>에러 발생: {error.message}</div>}>
-            <Suspense fallback={<div>로딩 중...</div>}>
-              <App />
-            </Suspense>
-          </ErrorBoundary>
-        ),
+        element: <App />,
       },
       {
         path: '/assembly',
