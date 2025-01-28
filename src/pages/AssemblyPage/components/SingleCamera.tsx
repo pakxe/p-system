9281e4004c0cbd8abd139a754a0005a0f8ac0a41
pakxe <pigkill40@naver.com>
2025-01-29 02:47:43 +0900

    feat: 컨트롤이 필요하다면 OrbitControls를 사용할 수 있도록 확장

--- src/pages/AssemblyPage/components/SingleCamera.tsx
+++ src/pages/AssemblyPage/components/SingleCamera.tsx
@@ -1,3 +1,4 @@
+import { OrbitControls } from '@react-three/drei';
 import { useThree } from '@react-three/fiber';
 import { PerspectiveCamera } from 'three';
 import * as THREE from 'three';
@@ -5,17 +6,19 @@ import * as THREE from 'three';
 type Props = {
   position: THREE.Vector3;
   size: number;
+  canControls?: boolean;
 };
 
-const SingleCamera = ({ position, size }: Props) => {
+const SingleCamera = ({ position, size, canControls }: Props) => {
   const { camera }: { camera: PerspectiveCamera } = useThree();
 
   const distance = size / 2;
+
   // 포지션에서 +z, +y, +x방향으로 20씩 떨어진 위치에서 본다,
   camera.position.set(position.x + distance, position.y + distance, position.z + distance);
   camera.lookAt(position);
 
-  return null;
+  return canControls ? <OrbitControls target={position} /> : null;
 };
 
 export default SingleCamera;
