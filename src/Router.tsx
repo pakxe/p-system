da2bd86b3dedddb16b2bd588452e46588a109ed7
pakxe <pigkill40@naver.com>
2025-01-29 01:54:00 +0900

    feat: 모달을 띄워주는 프로바이더 세팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,15 @@
 import { createBrowserRouter, Outlet } from 'react-router-dom';
 import App from './App';
 import AssemblyPage from './pages/AssemblyPage/AssemblyPage';
+import { ModalProvider } from './hooks/useModal';
 
 const router = createBrowserRouter([
   {
     path: '',
     element: (
-      <div>
+      <ModalProvider>
         <Outlet />
-      </div>
+      </ModalProvider>
     ),
     children: [
       {
