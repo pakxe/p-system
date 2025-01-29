db4a284b70f491a0974d872fcc0fcdc925524527
pakxe <pigkill40@naver.com>
2025-01-29 15:02:11 +0900

    feat: 부품 확인 모달을 띄우기 위한 훅

--- src/pages/AssemblyPage/hooks/usePartDetailModal.tsx
+++ src/pages/AssemblyPage/hooks/usePartDetailModal.tsx
@@ -6,8 +6,8 @@ const usePartDetailModal = (name: string) => {
   const { open } = useModalContext();
   return {
     open: () =>
-      open(MODAL_KEYS.ASSEMBLY_PART_DETAIL_INFO, (isOpen, onClose) => (
-        <PartDetailModal isOpen={isOpen} onClose={onClose} name={name} />
+      open(MODAL_KEYS.ASSEMBLY_PART_DETAIL_INFO, ({ isOpened, onClose }) => (
+        <PartDetailModal isOpened={isOpened} onClose={onClose} name={name} />
       )),
   };
 };
