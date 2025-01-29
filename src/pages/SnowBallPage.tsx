75c258d69e211fc8a255403d27ac65cadc2a094f
pakxe <pigkill40@naver.com>
2025-01-29 20:31:53 +0900

    fix: 간헐적으로 감속이 안되는 현상 제거

--- src/pages/SnowBallPage.tsx
+++ src/pages/SnowBallPage.tsx
@@ -1,8 +1,18 @@
 import { useEffect, useRef, useState } from 'react';
-import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber';
+import { Canvas, ThreeEvent, useFrame, useLoader, useThree } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 import NightSky from '../components/NightSky';
-import { Camera, DirectionalLightHelper, PCFSoftShadowMap, Quaternion, TextureLoader, Vector3 } from 'three';
+import {
+  BufferGeometry,
+  Camera,
+  DirectionalLightHelper,
+  Mesh,
+  NormalBufferAttributes,
+  PCFSoftShadowMap,
+  Quaternion,
+  TextureLoader,
+  Vector3,
+} from 'three';
 import snowSound from '../assets/snow.mp3';
 import snowSurface from '../assets/snow-surface.jpg';
 import CameraMoveLittle from '../components/CameraRig';
@@ -11,39 +21,42 @@ import { Bloom, EffectComposer } from '@react-three/postprocessing';
 import snowball from '../../public/modals/snowball.glb';
 import lb from '../../public/modals/loot_box.glb';
 
+const MIN_VELOCITY = 0.0001;
+const DEFAULT_X_Z = [0, 0];
+
 const Snowball = () => {
-  const snowballRef = useRef(); // 눈덩이 Mesh 참조
-  const [velocity, setVelocity] = useState([0, 0]); // 드래그 속도 (x, z)
+  const snowballRef = useRef();
+
   const [isDragging, setIsDragging] = useState(false); // 드래그 상태
-  const lastPointerPos = useRef([0, 0]); // 이전 포인터 위치
-  const { camera } = useThree() as { camera: Camera };
-  const [scale, setScale] = useState(1.3); // 눈덩이 크기 상태
-  const [position, setPosition] = useState([0, 0.8, 0]); // 눈덩이 위치
+  const lastPointerPos = useRef(DEFAULT_X_Z);
+
+  const [scale, setScale] = useState(1); // 눈덩이 크기 상태
+
+  const [velocity, setVelocity] = useState(DEFAULT_X_Z); // 드래그 속도 (x, z)
+  const [position, setPosition] = useState<[number, number, number]>([0, 0.8, 0]); // 눈덩이 위치
   const { scene } = useGLTF(snowball);
 
-  const texture = useLoader(TextureLoader, snowSurface);
-  // camera.position.y = 10;
-  // camera.position.z = 15;
-
-  const handleDragStart = (event) => {
+  const handleDragStart = (event: ThreeEvent<PointerEvent>) => {
     setIsDragging(true);
+
     lastPointerPos.current = [event.clientX, event.clientY];
   };
 
-  const handleDragging = (event) => {
+  // 드래그 중 = position, velocity 업데이트
+  const handleDragging = (event: ThreeEvent<PointerEvent>) => {
     if (isDragging && snowballRef.current) {
       const [lastX, lastY] = lastPointerPos.current;
       const deltaX = event.clientX - lastX;
       const deltaY = event.clientY - lastY;
 
-      lastPointerPos.current = [event.clientX, event.clientY];
-
-      const moveAmount = Math.abs(deltaX) + Math.abs(deltaY);
-      // setScale((prev) => prev + moveAmount * 0.0002);
+      if (deltaX === 0 && deltaY === 0) return; // 이유는 모르겠으나 0,0이 교차로 찍혀 움직여지지 않을 때가 있음. 그걸 피하기 위함
 
       setVelocity([deltaX * 0.02, deltaY * 0.02]);
-      setPosition((prev) => [prev[0] + deltaX * 0.015, prev[1], prev[2] + deltaY * 0.015]);
-      // setPosition((prev) => [prev[0] + deltaX * 0.005, prev[1] + moveAmount * 0.0002, prev[2] + deltaY * 0.005]);
+      setPosition((prev) => [prev[0] + deltaX * 0.01, prev[1], prev[2] + deltaY * 0.01]);
+
+      lastPointerPos.current = [event.clientX, event.clientY];
+      const moveAmount = Math.abs(deltaX) + Math.abs(deltaY);
+      // setScale((prev) => prev + moveAmount * 0.0002);
 
       const axis = new Vector3(deltaY, 0, -deltaX).normalize();
       const angle = (moveAmount / scale) * 0.005;
@@ -54,28 +67,27 @@ const Snowball = () => {
     }
   };
 
-  const handleDragEnd = (event) => {
+  // 드래그 끝 - 드래깅 상태 false로
+  const handleDragEnd = (event: ThreeEvent<PointerEvent>) => {
     setIsDragging(false);
   };
 
-  const audioRef = useRef(null);
-
+  // 감속
   useFrame(() => {
-    if (!isDragging && velocity[0] !== 0 && velocity[1] !== 0) {
+    if (!isDragging && velocity[0] !== 0 && velocity[1] !== 0 && snowballRef.current) {
       const deltaX = velocity[0];
       const deltaZ = velocity[1];
       const moveAmount = Math.abs(deltaX) + Math.abs(deltaZ);
 
-      setPosition((prev) => [prev[0] + deltaX, prev[1], prev[2] + deltaZ]);
+      setPosition((prev) => [prev[0] + velocity[0], prev[1], prev[2] + velocity[1]]);
 
-      setVelocity((prev) => [prev[0] * 0.8, prev[1] * 0.8]);
+      setVelocity((prev) => [prev[0] * 0.6, prev[1] * 0.6]);
 
-      if (Math.abs(velocity[0]) < 0.0001 && Math.abs(velocity[1]) < 0.0001) {
+      if (Math.abs(velocity[0]) < MIN_VELOCITY && Math.abs(velocity[1]) < MIN_VELOCITY) {
         setVelocity([0, 0]);
       }
-
       const axis = new Vector3(deltaZ, 0, -deltaX).normalize();
-      const angle = moveAmount / scale;
+      const angle = moveAmount;
 
       const quaternion = new Quaternion();
       quaternion.setFromAxisAngle(axis, angle);
@@ -83,38 +95,40 @@ const Snowball = () => {
     }
   });
 
-  const fadeOutAudio = (audio) => {
-    const fadeInterval = 100; // 볼륨을 줄일 간격(ms)
-    const fadeStep = 0.01; // 볼륨 감소량
+  const audioRef = useRef(null);
 
-    const fadeOut = setInterval(() => {
-      if (audio.volume > 0) {
-        audio.volume = Math.max(0, audio.volume - fadeStep); // 볼륨 감소
-      } else {
-        clearInterval(fadeOut); // 볼륨이 0이 되면 타이머 정리
-        audio.pause(); // 소리 멈춤
-      }
-    }, fadeInterval);
-  };
+  // const fadeOutAudio = (audio) => {
+  //   const fadeInterval = 100; // 볼륨을 줄일 간격(ms)
+  //   const fadeStep = 0.01; // 볼륨 감소량
 
-  useEffect(() => {
-    if (!audioRef.current) {
-      audioRef.current = new Audio(snowSound);
-      audioRef.current.loop = true; // 소리를 반복 재생
-    }
+  //   const fadeOut = setInterval(() => {
+  //     if (audio.volume > 0) {
+  //       audio.volume = Math.max(0, audio.volume - fadeStep); // 볼륨 감소
+  //     } else {
+  //       clearInterval(fadeOut); // 볼륨이 0이 되면 타이머 정리
+  //       audio.pause(); // 소리 멈춤
+  //     }
+  //   }, fadeInterval);
+  // };
 
-    if (isDragging) {
-      audioRef.current.volume = 1;
-      audioRef.current.play();
-    } else {
-      fadeOutAudio(audioRef.current);
-    }
+  // useEffect(() => {
+  //   if (!audioRef.current) {
+  //     audioRef.current = new Audio(snowSound);
+  //     audioRef.current.loop = true; // 소리를 반복 재생
+  //   }
 
-    return () => {
-      // 컴포넌트 언마운트 시 정리
-      audioRef.current?.pause();
-    };
-  }, [isDragging]);
+  //   if (isDragging) {
+  //     audioRef.current.volume = 1;
+  //     audioRef.current.play();
+  //   } else {
+  //     fadeOutAudio(audioRef.current);
+  //   }
+
+  //   return () => {
+  //     // 컴포넌트 언마운트 시 정리
+  //     audioRef.current?.pause();
+  //   };
+  // }, [isDragging]);
 
   return (
     <mesh
@@ -126,15 +140,14 @@ const Snowball = () => {
       onPointerDown={handleDragStart}
       onPointerMove={handleDragging}
       onPointerUp={handleDragEnd}>
-      <primitive object={scene} />
-      <sphereGeometry args={[scale, 32, 32]} />
+      <primitive object={scene} scale={[scale, scale, scale]} />
+      <sphereGeometry args={[scale + 0.2, 32, 32]} />
     </mesh>
   );
 };
 
 const SnowBallPage = () => {
   const texture = useLoader(TextureLoader, snowSurface);
-  const cameraRef = useRef(); // 카메라 참조
 
   return (
     <Canvas
@@ -149,22 +162,20 @@ const SnowBallPage = () => {
         shadow-camera-right={25}
         shadow-mapSize={2048}
         castShadow
-        position={[-1, 100, 10]} // 조명 위치
-        intensity={1.5} // 조명 강도
+        position={[-1, 100, 10]}
+        intensity={1.5}
       />
-      <ambientLight intensity={0.3} /> {/* 주변광 */}
+      <ambientLight intensity={0.3} />
       <Snowball />
       <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
         <planeGeometry args={[100, 100]} />
         <meshStandardMaterial map={texture} />
       </mesh>
       <axesHelper />
-      {/* <perspectiveCamera ref={cameraRef} /> */}
       <CameraRig />
-      {/* <OrbitControls /> */}
-      <EffectComposer>
+      {/* <EffectComposer>
         <Bloom kernelSize={3} luminanceThreshold={0} luminanceSmoothing={0.1} intensity={0.8} />
-      </EffectComposer>
+      </EffectComposer> */}
     </Canvas>
   );
 };
