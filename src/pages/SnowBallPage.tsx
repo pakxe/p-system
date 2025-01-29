a5fdcb936cec7cdd91cd1603efed8078c476affd
pakxe <pigkill40@naver.com>
2025-01-29 21:54:10 +0900

    feat: 조명을 더 어둡게 수정

--- src/pages/SnowBallPage.tsx
+++ src/pages/SnowBallPage.tsx
@@ -10,7 +10,8 @@ import setQuaternion from '../utils/setQuaternion';
 import Button from '../components/Button';
 import { css } from '@emotion/react';
 import { VECTOR_3 } from '../types';
-// import { Bloom, EffectComposer } from '@react-three/postprocessing';
+import Snowfall from '../components/SnowFall';
+import { Bloom, EffectComposer } from '@react-three/postprocessing';
 
 const MIN_VELOCITY = 0.0001;
 const DEFAULT_X_Z = [0, 0];
@@ -151,7 +152,7 @@ const SnowBallPage = () => {
         shadows={{ type: PCFSoftShadowMap }}
         camera={{ position: [0, 5, 5.5], fov: 50 }}
         style={{ height: '100vh', width: '100vw' }}>
-        <ambientLight intensity={0.4} />
+        <ambientLight intensity={0.2} /> {/* 주변 조명을 어둡게 */}
         <directionalLight
           shadow-camera-top={25}
           shadow-camera-bottom={-25}
@@ -159,20 +160,20 @@ const SnowBallPage = () => {
           shadow-camera-right={25}
           shadow-mapSize={2048}
           castShadow
-          position={[-1, 100, 10]}
-          intensity={1.5}
+          position={[-1, 50, 20]} // 하늘에서 비추는 느낌
+          intensity={1.2} // 기본 조명보다 약간 어둡게
+          color={'#84a4e5'} // 파란빛 (밤하늘 느낌)
         />
-        <ambientLight intensity={0.3} />
+        {/* 주변을 은은하게 비추는 조명 */}
+        <pointLight position={[0, 30, 0]} intensity={10} color={'#f88a72'} distance={100} decay={0.8} />
+        <fog attach='fog' args={['#0a0a1a', 10, 80]} />
         <Snowball triggerReset={triggerReset} />
         <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
           <planeGeometry args={[100, 100]} />
           <meshStandardMaterial map={texture} />
         </mesh>
-        <axesHelper />
+        <Snowfall />
         <CameraRig />
-        {/* <EffectComposer>
-        <Bloom kernelSize={3} luminanceThreshold={0} luminanceSmoothing={0.1} intensity={0.8} />
-      </EffectComposer> */}
       </Canvas>
       <div
         css={css`
