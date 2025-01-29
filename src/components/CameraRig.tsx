1507729d7a96afddfe15f737cf762900327fb80e
pakxe <pigkill40@naver.com>
2025-01-29 21:32:44 +0900

    feat: 마우스따라 움직이는 카메라가 더 둔하게 움직이도록 수정

--- src/components/CameraRig.tsx
+++ src/components/CameraRig.tsx
@@ -5,7 +5,7 @@ function CameraRig() {
   useFrame((state, delta) => {
     easing.damp3(
       state.camera.position,
-      [-1 + (state.pointer.x * state.viewport.width) / 3, 15 + state.pointer.y / 2, 15],
+      [-1 + (state.pointer.x * state.viewport.width) / 6, 15 + state.pointer.y / 4, 15],
       0.8,
       delta,
     );
