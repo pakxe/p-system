3262beb5a32a1cf410969723b2cfe803882e1e13
pakxe <pigkill40@naver.com>
2025-01-29 22:23:14 +0900

    feat: 행성을 더 가까이 비추도록 수정

--- src/components/CameraMover.tsx
+++ src/components/CameraMover.tsx
@@ -71,7 +71,7 @@ function CameraMover({ selectedSystemObjectRef, isExplore }: Props) {
     }
 
     // 카메라 위치를 타겟 궤도로 부드럽게 이동
-    const targetVector = new Vector3(targetPosition.x, targetPosition.y + 3, targetPosition.z + 10);
+    const targetVector = new Vector3(targetPosition.x, targetPosition.y + 2, targetPosition.z + 6);
     currentPos.current.lerp(targetVector, lerpSpeed);
     camera.position.copy(currentPos.current);
 
